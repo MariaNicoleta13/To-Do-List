e84ab8b491b773ca09da122606b1769b3a64554c
Alin Dumitru <alin.dumitru20@gmail.com>
2020-01-09 21:10:25 +0200

    added dummy server

--- client/app.js
+++ client/app.js
@@ -1,15 +1,15 @@
-var express=require("express");
-var app=express();
+var express = require('express');
+var app = express();
 
 //app.use(express.static("public"));
 app.use(express.static(__dirname + '/public'));
 
-app.set("view engine","ejs");
+app.set('view engine', 'ejs');
 
-app.get("/",function(require,response){
-    response.render("index.ejs");
+app.get('/', function(require, response) {
+    response.render('index.ejs');
 });
 
-app.listen(3000,function(){
-   console.log("server is running");
+app.listen(3000, function() {
+    console.log('server is running');
 });
