833f06dbd4fa9a1c1f6a9b4acfde0a8f2c65fc70
Mary <maria.nicoleta.ion13@gmail.com>
2020-01-15 21:50:23 +0200

    initial styling

--- client/app.js
+++ client/app.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var app = express();
-
+var request=require("request");
+var url="http://localhost:3001/notes";
 //app.use(express.static("public"));
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(require, response) {
-    response.render('index.ejs');
+app.get('/', function(req, res) {
+    request(url,(error,response,body)=>{
+        if(!error&&response.statusCode==200){
+             var list=JSON.parse(body);
+            // console.log(list);
+            res.render('index.ejs',{listToUpload:list});
+        }
+    });
+    
+
 });
+
+
 
 app.listen(3000, function() {
     console.log('server is running');
